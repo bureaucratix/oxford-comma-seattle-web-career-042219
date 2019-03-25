aef5d878b2e10d1b4f8475a742bcedd80371a669
Alex Kitelinger <bureaucratix@gmail.com>
2019-03-24 20:12:59 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array.join
+  elsif array.length == 2
+    string = array.join(" and ")
+  else
+  final_item = array.pop
+  string = array.join(", ")
+  string << ", and #{final_item.to_s}"
+  end
+  string
 end
+
+puts oxford_comma(["Hello"])
+
+# commastring = ""
+# for index in 0...array.length-1
+#   puts array[index]
+# end
